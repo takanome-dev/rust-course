f0e69807d9b16645d2f722f1f63235270d472e9d
takanome_dev <takanomedev221@gmail.com>
2023-05-20 16:16:01 +0000

    iterating through a text file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-// Coding an iterator, check -> `main.rs` file
-const arr = [1, 2, 3].map(x => x + 1);
+import fs from "fs";
+import path from "path";
 
-console.log({ arr });
+// Read the file `lines` in the root of the project
+// and print each line to the console
+
+// Do it this way
+fs.readFileSync("lines")
+  .toString()
+  .split("\n")
+  .filter((_, i) => i % 2 === 0)
+  .forEach(line => {
+    console.log(line);
+  });
+
+// Or this way
+fs.readFileSync(path.join(__dirname, "../lines"), {
+  encoding: "utf-8",
+})
+  .split("\n")
+  .filter((_, i) => i % 2 === 0)
+  .forEach(line => {
+    console.log(line);
+  });
+
+// Or I don't know :)
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,9 @@
-// Coding an iterator
-fn iterator() {
-    let items: Vec<_> = vec![1, 2, 3]
-        .iter()// create the iterator to go over the elements in teh array
-        .map(|x| x + 1) // do the plus one'ings
-        .collect(); // take the iterator and put it somewhere..
+fn main() {
+  let file = std::fs::read_to_string("lines").unwrap();
 
-    println!("items {:?}", items);
+  file
+    .lines()
+    .enumerate()
+    .filter(|(idx,_)| idx % 2 == 0)
+    .for_each(|(_, line)| println!("{}", line));
 }
-
-// This is how `collect` works under the hood
-fn how_collect_work() {
-    // data lives for the duration of main
-    let data = vec![1, 2, 3];
-
-    let mut foo = data.iter().map(|x| x + 1);
-
-    // it creates a new vector (it should be mutable)
-    let mut new_vector = vec![];
-
-    // go through the iterator(foo) one at the time
-    while let Some(x) = foo.next() {
-        // push the mapped value into the new vector 
-        new_vector.push(x)
-    }
-
-    println!("foo {:?}", new_vector)
-}
-
-
-fn test_your_understanding() {
-    // What is value?
-    let value: usize = vec![1, 2, 3]
-        .iter()
-        .sum();
-
-    println!("sum {:?}", value); // expected to be 6
-
-    let how_many_times: usize = vec![1, 2, 3]
-    .iter()
-    .skip(2)
-    .count();
-
-    println!("count {:?}", how_many_times); // expected to be 1
-
-    // what will i print?
-    vec![1,2,5,9,4]
-    .iter()
-    .skip(2)
-    .take_while(|&&x| x > 4)
-    .for_each(|x| println!("{}", x)); // will print 5 and 9
-
-    let what_about_this: usize = vec![1, 2, 3]
-        .iter()
-        .filter(|x|  *x % 2 == 0)
-        .count();
-
-    println!("what_about_this {:?}", what_about_this); // return 1
-}
-
-fn main() {}
