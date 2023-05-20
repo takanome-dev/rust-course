89f7f211f1404d41cd7bacd9780d37ae7c78a6fe
takanome_dev <takanomedev221@gmail.com>
2023-05-20 10:43:28 +0000

    coding an iterator

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Coding an iterator, check -> `main.rs` file
+const arr = [1, 2, 3].map(x => x + 1);
+
+console.log({ arr });
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Coding an iterator
 fn main() {
-    println!("Hello, world!");
+    let items: Vec<isize> = vec![1, 2, 3]
+        .iter() // create the iterator to go over the elements in teh array
+        .map(|x| x + 1) // do the plus one'ings
+        .collect(); // take the iterator and put it somewhere..
+
+    println!("items {:?}", items);
 }
